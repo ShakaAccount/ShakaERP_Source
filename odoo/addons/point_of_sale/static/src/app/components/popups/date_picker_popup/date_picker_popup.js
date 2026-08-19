ae4dddd40422e2cd17ea2458fea54fd5c31c29df
russellzparadox <russellzparadox@gmail.com>
2026-08-19 11:11:05 +0330

    changed gregorian date picker hopefully

--- odoo/addons/point_of_sale/static/src/app/components/popups/date_picker_popup/date_picker_popup.js
+++ odoo/addons/point_of_sale/static/src/app/components/popups/date_picker_popup/date_picker_popup.js
@@ -1,6 +1,7 @@
 import { Dialog } from "@web/core/dialog/dialog";
 import { _t } from "@web/core/l10n/translation";
 import { Component, onMounted, useRef, useState } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
 
 export class DatePickerPopup extends Component {
     static template = "point_of_sale.DatePickerPopup";
@@ -18,17 +19,38 @@ export class DatePickerPopup extends Component {
 
     setup() {
         super.setup();
+        this.datetimePicker = useService("datetime_picker");
+        this.popupService = useService("popover");
         this.state = useState({ shippingDate: this._today() });
         this.inputRef = useRef("input");
         onMounted(() => this.inputRef.el.focus());
     }
-    confirm() {
-        this.props.getPayload(
-            this.state.shippingDate < this._today() ? this._today() : this.state.shippingDate
-        );
+
+    async confirm() {
+        const date = this.state.shippingDate < this._today() ? this._today() : this.state.shippingDate;
+        this.props.getPayload(date);
         this.props.close();
     }
+
     _today() {
         return new Date().toISOString().split("T")[0];
+    }
+
+    async openDatePicker() {
+        const picker = this.datetimePicker.create({
+            target: this.inputRef.el,
+            pickerProps: {
+                type: "date",
+                value: this.state.shippingDate ? luxon.DateTime.fromISO(this.state.shippingDate) : null,
+                minDate: luxon.DateTime.local().startOf("day"),
+            },
+            onApply: (value) => {
+                if (value) {
+                    this.state.shippingDate = value.toISODate();
+                }
+            },
+        });
+        picker.enable();
+        picker.open(0);
     }
 }
